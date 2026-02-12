324793de4b433a15127f3f4e4b0f730e9d7f6d58
antoniovicentetravassos-arch <antoniovicentetravassos@gmail.com>
2026-02-12 16:56:47 +0000

    Update api_noticias.py

--- api_noticias.py
+++ api_noticias.py
@@ -41,5 +41,8 @@ def noticias():
 def imagens():
     return jsonify(obter_imagens())
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
